2c8ef8cd4f12def7b5883aa220ea94be4d580b2b
Nicholas Curtis <nich.curtis@gmail.com>
2012-03-22 17:10:20 -0700

    updates

--- CQB.py
+++ CQB.py
@@ -4,6 +4,7 @@ import sys, wx
 
 import query
 import connection
+from connection import errors
 
 class CQB(wx.App):
 	''' '''
@@ -11,9 +12,13 @@ class CQB(wx.App):
 	def OnInit(self):
 		''' '''
 		
-		con = connection.CQBConnection.instance(wx.ID_ANY, 'mysql')
-		con.connect(host='10.182.227.26', user='youcallmd', passwd='19u8hf9quh')
-		con.set_db('youcallmd')\
+		try:
+			con = connection.CQBConnection.instance(wx.ID_ANY, 'mysql')
+			con.connect(host='10.182.227.26', user='youcallmd', passwd='19u8hf9quh')
+			con.set_db('youcallmd')
+		except errors.CQBConnectionError:
+			result = wx.MessageBox("You do not have an active connection!", "Connection Failed", wx.OK | wx.ICON_ERROR)
+			return False
 		
 		self.mainFrame = query.getNewQueryBrowser(connection=con, parent=None)
 		self.SetTopWindow(self.mainFrame)
--- query/__init__.py
+++ query/__init__.py
@@ -38,6 +38,47 @@ class CQBQueryEventStop(CQBQueryEvent):
 EVT_CQB_QRY_STOP_ID = wx.NewEventType()
 EVT_CQB_QRY_STOP = wx.PyEventBinder(EVT_CQB_QRY_STOP_ID, 1)
 
+class CQBQueryResult(wx.grid.PyGridTableBase):
+	'''	'''
+	
+	def __init__ (self, field_names, results, *args, **kwargs):
+		super(CQBQueryResult, self).__init__(*args, **kwargs)
+		self.field_names = field_names
+		self.results = results
+	
+	def GetNumberRows(self):
+		return len(self.results)
+	
+	def GetNumberCols(self):
+		return len(self.results[0])
+	
+	def IsEmptyCell(self, row, col):
+		row = self.results[row]
+		if row[col]:
+			return False
+		return True
+	
+	def GetValue(self, row, col):
+		row = self.results[row]
+		return row[col]
+	
+	def SetValue(self, row, col, value):
+		pass
+	
+	def GetColLabelValue(self, col):
+		if self.field_names:
+			return self.field_names[col]
+	
+	def GetRowLabelValue(self, row):
+		return row
+	
+class CQBQueryResultGrid(wx.grid.Grid):
+	''' '''
+	
+	def __init__ (self, field_names=(), results=[], *args, **kwargs):
+		super(CQBQueryResultGrid, self).__init__(*args, **kwargs)
+		self.SetTable(CQBQueryResult(field_names, results))
+	
 class CQBQueryCtrl(wx.TextCtrl):
 	''' '''
 	
@@ -104,48 +145,6 @@ class CQBQueryController(wx.EvtHandler):
 		''' '''
 		
 		e.Skip()
-	
-
-class CQBQueryResult(wx.grid.PyGridTableBase):
-	'''	'''
-	
-	def __init__ (self, field_names, results, *args, **kwargs):
-		super(CQBQueryResult, self).__init__(*args, **kwargs)
-		self.field_names = field_names
-		self.results = results
-	
-	def GetNumberRows(self):
-		return len(self.results)
-	
-	def GetNumberCols(self):
-		return len(self.results[0])
-	
-	def IsEmptyCell(self, row, col):
-		row = self.results[row]
-		if row[col]:
-			return False
-		return True
-	
-	def GetValue(self, row, col):
-		row = self.results[row]
-		return row[col]
-	
-	def SetValue(self, row, col, value):
-		pass
-	
-	def GetColLabelValue(self, col):
-		if self.field_names:
-			return self.field_names[col]
-	
-	def GetRowLabelValue(self, row):
-		return row
-	
-class CQBQueryResultGrid(wx.grid.Grid):
-	''' '''
-	
-	def __init__ (self, field_names=(), results=[], *args, **kwargs):
-		super(CQBQueryResultGrid, self).__init__(*args, **kwargs)
-		self.SetTable(CQBQueryResult(field_names, results))
 	
 
 class CQBQueryBrowser(wx.Frame, CQBQueryEvent):
